835b1d07e0c431ab47419deea6dbef0393f54beb
figraham <fi.sage.graham@gmail.com>
2019-04-08 19:12:27 -0600

    🏗️ version: 0.0.0

--- dist/core/hello.d.ts
+++ dist/core/hello.d.ts
@@ -1,2 +1,14 @@
+/**
+ * Logs hello message to the console.
+ *
+ * @param text Will be output after "Hello "
+ */
 export declare function sayHello(text: string): void;
+/**
+ * Adds firstNumber and secondNumber using [[add]] then will output the result
+ * to console with the form "Hello (added number)".
+ *
+ * @param firstNumber
+ * @param secondNumber
+ */
 export declare function addHello(firstNumber: number, secondNumber: number): void;
--- dist/math/math.d.ts
+++ dist/math/math.d.ts
@@ -1,1 +1,8 @@
+/**
+ * Simple alias for adding two numbers.
+ *
+ * @param firstNumber
+ * @param secondNumber
+ * @returns firstNumber added to secondNumber
+ */
 export declare function add(firstNumber: number, secondNumber: number): number;
